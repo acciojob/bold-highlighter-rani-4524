0f2afa556366e6179ea4143e6017a5ee519abfdc
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-11-24 16:28:42 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
-
+	const highlightedFunction = document.querySelectorAll('strong');
+	highlightedFunction.forEach(item=>{
+		item.style.color="rgb(0, 128, 0)";
+});
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	const normalFunction=document.querySelectorAll("strong");
+	normalFunction.forEach(item=>{
+		item.style.color="rgb(0, 0, 0)";
+}); 
 }
